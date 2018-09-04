e9f3713a61b393ae45fd6727ba78a9b9dd98781a
rifkegribenes <rifkegribenes@gmail.com>
2018-09-03 20:30:13 -0700

    masonry styles

--- client/src/components/ImageGrid.jsx
+++ client/src/components/ImageGrid.jsx
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Masonry from "react-masonry-component";
-// import Card from '@material-ui/core/Card';
-// import CardActionArea from '@material-ui/core/CardActionArea';
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
 // import CardActions from '@material-ui/core/CardActions';
 // import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
+import CardMedia from "@material-ui/core/CardMedia";
 // import IconButton from "@material-ui/core/IconButton";
 import withWidth, { isWidthUp } from "@material-ui/core/withWidth";
 // import pinIcon from "../img/pin.svg";
@@ -16,17 +16,22 @@ const styles = theme => ({
   root: {
     margin: "0 auto",
     width: "100%",
-    maxWidth: 1500
+    maxWidth: 1920
   },
   gridList: {},
   card: {
     // this class is being used directly, so can't set options in JSS
   },
-  media: {
-    objectFit: "cover"
-  },
   masonry: {
     margin: "0 auto"
+  },
+  caption: {
+    padding: 10
+  },
+  media: {
+    width: 300,
+    height: "auto",
+    borderRadius: "4px 4px 0 0"
   },
   icon: {
     color: theme.palette.primary.main
@@ -37,11 +42,18 @@ const styles = theme => ({
   }
 });
 
+const cardStyle = {
+  marginBottom: 10,
+  borderRadius: 4,
+  width: 300
+};
+
 const ImageGrid = props => {
   const { classes } = props;
   const masonryOptions = {
     itemSelector: ".card",
     gutter: 10,
+    columnWidth: 300,
     fitWidth: true
   };
   return (
@@ -55,13 +67,18 @@ const ImageGrid = props => {
       <Masonry options={masonryOptions} className={classes.masonry}>
         {/*        <div className="grid-sizer" style={{ width: "160px"}}/>*/}
         {props.tileData.map(tile => (
-          <img
-            className="card"
-            style={{ marginBottom: 10 }}
-            src={tile.url}
-            alt={tile.snippet}
-            key={tile.id}
-          />
+          <div className="card" style={cardStyle} key={tile.id}>
+            <CardActionArea>
+              <img
+                className={classes.media}
+                src={tile.url}
+                alt={tile.snippet}
+              />
+              <Typography component="p" className={classes.caption}>
+                {tile.snippet}
+              </Typography>
+            </CardActionArea>
+          </div>
         ))}
       </Masonry>
     </div>
